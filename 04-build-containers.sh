7a7c98ebfb0bde86c0766fb0831bf783978f7dd9
rlucente <rlucente@redhat.com>
2021-04-17 17:10:33 -0400

    make web server output more interesting

--- 04-build-containers.sh
+++ 04-build-containers.sh
@@ -3,21 +3,16 @@
 . $(dirname $0)/demo.conf
 
 #
-# We need to add the $HOSTIP to the network interface to push the
-# images to our server. This same address will be available when the
-# edge guest is running later with SLiRP networking.
+# Something a bit more interesting than just plain text
 #
-ETHDEV=$(ip route get 8.8.8.8 | sed 's/.*dev //g' | awk '{print $1;exit}')
-sudo nmcli con mod $ETHDEV +ipv4.addresses $HOSTIP/24
-sudo nmcli con down $ETHDEV
-sudo nmcli con up $ETHDEV
+sudo dnf -y install cowsay figlet
 
 #
 # Create containerized httpd application version 1
 #
 CTR_ID=$(buildah from registry.access.redhat.com/ubi8/ubi:latest)
 buildah run $CTR_ID -- yum -y install httpd
-echo 'Welcome to RHEL for Edge!' > index.html
+echo "RHEL for Edge" | figlet > index.html
 buildah copy $CTR_ID index.html /var/www/html/index.html
 buildah config --cmd "/usr/sbin/httpd -D FOREGROUND" $CTR_ID
 buildah config --port 80 $CTR_ID
@@ -35,7 +30,7 @@ podman push $HOSTIP:5000/httpd:prod
 # Create containerized httpd application version 2
 #
 CTR_ID=$(buildah from $HOSTIP:5000/httpd:v1)
-echo "Podman auto-update is awesome!" >> index.html
+cowthink -f tux 'Podman auto-update is awesome!' >> index.html
 buildah copy $CTR_ID index.html /var/www/html/index.html
 buildah commit $CTR_ID $HOSTIP:5000/httpd:v2
 
