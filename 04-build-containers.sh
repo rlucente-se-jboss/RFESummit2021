b086a934b2ff832b9a6964728455e3a2b2833f36
rlucente <rlucente@redhat.com>
2021-04-18 18:43:37 -0400

    use EPEL to install figlet and cowsay

--- 04-build-containers.sh
+++ 04-build-containers.sh
@@ -3,9 +3,17 @@
 . $(dirname $0)/demo.conf
 
 #
-# Something a bit more interesting than just plain text
+# Use EPEL to install something a bit more interesting than just
+# plain text
 #
-sudo dnf -y install cowsay figlet
+sudo subscription-manager repos \
+    --enable=codeready-builder-for-rhel-8-x86_64-rpms
+
+sudo dnf -y install dnf-utils \
+    https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+sudo dnf config-manager --disable epel epel-modular
+
+sudo dnf -y install --enablerepo=epel cowsay figlet || exit 1
 
 #
 # Create containerized httpd application version 1
