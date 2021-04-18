10dd52b2fb1593876f79b1ac0e85cdd7d5fa653d
rlucente <rlucente@redhat.com>
2021-04-18 19:08:32 -0400

    switch to libvirt

--- 06-install-edge-guest.sh
+++ 06-install-edge-guest.sh
@@ -3,21 +3,15 @@
 . $(dirname $0)/demo.conf
 
 #
-# Create an empty disk for install
+# Launch virtual edge device but use bridged networking
 #
-rm -f edge-disk.qcow2
-qemu-img create -f qcow2 edge-disk.qcow2 $HDD_SIZE
-
-#
-# QEMU is running in user space with SLiRP networking. All output
-# is redirected to the user terminal.
-#
-qemu-system-x86_64 \
-    -serial mon:stdio \
-    -nographic \
-    -m $MEM_SIZE \
-    -device virtio-net-pci,netdev=n0 \
-    -netdev user,id=n0,net=$VM_NET \
-    -drive file=edge-disk.qcow2 \
-    -cdrom bootwithks.iso
+virt-install \
+    --name edge-device \
+    --memory $MEM_SIZE \
+    --vcpus $NUM_CPUS \
+    --network bridge=bridge0 \
+    --location bootwithks.iso \
+    --os-variant=$OS_VARIANT \
+    --disk size=$HDD_SIZE \
+    --graphics none
 
